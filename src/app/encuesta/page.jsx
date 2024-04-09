451cbcbd56387b6dee3d0123b65ffdf7b5e4c6a1
elychirivella10 <elychirivella10@gmail.com>
2024-04-09 00:37:23 -0400

    creacion y configuracion del proyecto

---
+++ src/app/encuesta/page.jsx
@@ -0,0 +1,18 @@
+import Panel from "@/components/panel/Panel";
+
+ const Form =()=> {
+  return (
+    <div className="columns is-multiline is-centered">
+      <div className="column is-10">
+        <Panel title='SAPI' subtitle={`Encuesta`}/>
+      </div>
+      <div className="column is-10">
+        <div className="box">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Form
---
+++ src/app/layout.jsx
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+
+import '@/styles/style.css'
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "CNE",
+  description: "Pagina para ingresar datos de votacion",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="container mt-4">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
