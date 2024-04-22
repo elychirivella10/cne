608d5173ed7bfc6287b06b16f11031585940e277
elychirivella10 <elychirivella10@gmail.com>
2024-04-22 23:05:32 +0200

    cambios ruta

--- src/lib/helpers/variablesGoblales.js
+++ src/lib/helpers/variablesGoblales.js
@@ -1,3 +1,3 @@
 //export const rutaAxios = "https://minaguas.com/";
 //export const rutaAxios = "https://censoapi.sapi.gob.ve/api/"
-export const rutaAxios = "http://localhost:8080/"
+export const rutaAxios = "https://cnebackend2.seguimientoeva.com/"
